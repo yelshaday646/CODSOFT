b1af0c397561d8ad252bef902268669278a45c31
zeelshaday kibru <59030145+yelshaday646@users.noreply.github.com>
2024-03-06 17:51:15 +0300

    work on home page and about me
    work on home page and about me most of html and css

---
+++ script.js
@@ -0,0 +1,11 @@
+var links = document.getElementsByClassName("infoLink");
+var contents = document.getElementsByClassName("infoContents");
+function displayContent(display){
+    for(info-links of links){
+        info-links.classList.remove("active-link");
+    }
+    for (info-contents of contents) {
+      info-contents.classList.remove("activeContents");
+    }
+}
+alert("hello");
